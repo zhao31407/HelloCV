42c15a10818b4bb0052f3dde475432f7f1afa2a4
zhao <2926945960@qq.com>
2025-10-26 00:23:21 +0800

    delete

--- helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    cout << "欢迎学习 C++ 编程！" << endl;
-    return 0;
-}
--- key.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-int main(){
-    cin>>"请输入要加密的文本">>file;
-    std::string originalText =
-}
