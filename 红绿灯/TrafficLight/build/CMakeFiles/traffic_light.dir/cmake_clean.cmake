2d792c3a9a5dce28fa93748f25397b023a89d748
zhao31407 <2926945960@qq.com>
2025-10-26 11:22:49 +0800

    Add files via upload

---
+++ 红绿灯/TrafficLight/build/CMakeFiles/traffic_light.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traffic_light.dir/main.cpp.o"
+  "CMakeFiles/traffic_light.dir/main.cpp.o.d"
+  "CMakeFiles/traffic_light.dir/video_process.cpp.o"
+  "CMakeFiles/traffic_light.dir/video_process.cpp.o.d"
+  "traffic_light"
+  "traffic_light.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/traffic_light.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
